1d342d1c8aafbfa8475499efdcc28baa9a122338
Steph <1006462@student.sae.edu>
2017-09-29 18:33:55 +1000

    added objects, temp sprites, scrolling script, audio manager, and blank game manager script

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    AudioSource sounds;
+
+    // Use this for initialization
+    void Start()
+    {
+        instance = this;
+        sounds = GetComponent<AudioSource>();
+    }
+
+    public void Engine()
+    {
+
+    }
+
+    public void EngineWobble()
+    {
+
+    }
+
+    public void Break()
+    {
+
+    }
+
+    public void Collision()
+    {
+
+    }
+
+    public void BarnEnter()
+    {
+
+    }
+
+    public void CircleExplode()
+    {
+
+    }
+
+    public void ButtonChoose()
+    {
+
+    }
+
+    public void ButtonPress()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ObjectScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeX;
+
+    public Vector3 startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPos + Vector3.left * newPos;
+    }
+}
